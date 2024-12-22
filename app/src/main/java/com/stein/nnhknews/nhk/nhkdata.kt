6fcd7f65faf36b3d4fe36faa61806489ee201b34
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-12-22 18:10:50 +0900

    chore: some ui adjust

--- app/src/main/java/com/stein/nnhknews/nhk/nhkdata.kt
+++ app/src/main/java/com/stein/nnhknews/nhk/nhkdata.kt
@@ -17,38 +17,6 @@ data class NhkNews(
         val publishedAtUtc: String,
 )
 
-const val EMPTY_HTML =
-        """
-   <!DOCTYPE html>
-   <html>
-     <head>
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     </head>
-     <body>
-       <style>
-         * {
-           font-size: 16px;
-         }
-         h1 {
-           font-size: 20px;
-         }
-         rt {
-           font-size: 12px;
-         }
-         img {
-           max-width: 100%;
-         }
-         .under {
-           text-decoration-line: underline;
-           -webkit-text-decoration-line: underline;
-           text-decoration-color: #ff7f00;
-           -webkit-text-decoration-color: #ff7f00;
-         }
-       </style>
-     </body>
-   </html>
-   """
-
 fun NhkNews.newsHtml(): String {
     var image = ""
     if (imageUrl != "") {
@@ -56,13 +24,16 @@ fun NhkNews.newsHtml(): String {
     }
 
     return """
-          <!DOCTYPE html>
+      <!DOCTYPE html>
       <html>
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
         </head>
         <body>
           <style>
+            body {
+              padding-bottom: 40px;
+            }
             * {
               font-size: 16px;
             }
