aa793356e0116f6b6f02f8bec065620f3833173d
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-12-24 13:28:30 +0900

    chore: bump deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.stein.nnhknews"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.stein.nnhknews"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.7"
 
